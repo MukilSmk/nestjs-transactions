6e78884a8418413ca02752dcb9f5e0d63588a9b4
MukilKumar <mukilselvam27@gmail.com>
2023-02-02 10:31:29 +0530

    Injected transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TransactionModule } from './transactions/transaction.module';
 
 @Module({
-  imports: [ MongooseModule.forRoot('mongodb://localhost/bulk_transaction')  ],
+  imports: [ MongooseModule.forRoot('mongodb://localhost/bulk_transaction') , TransactionModule ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/transactions/transaction.module.ts
+++ src/transactions/transaction.module.ts
@@ -2,9 +2,14 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { Mongoose } from "mongoose";
 import { Transaction, TransactionSchema } from "./schemas/transaction.schema";
+import { TransactionController } from "./transaction.controller";
+import { TransactionRepository } from "./transaction.repository";
+import { TransactionsService } from "./transaction.service";
 
 @Module({
-    imports : [MongooseModule.forFeature([{name: Transaction.name, schema: TransactionSchema}])]
+    imports : [MongooseModule.forFeature([{name: Transaction.name, schema: TransactionSchema}])],
+    controllers: [TransactionController],
+    providers:[TransactionsService, TransactionRepository]
 })
 
 export class TransactionModule {
